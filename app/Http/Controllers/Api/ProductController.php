3d4cb415c5ff8f7525e1ce7d44fb8f6e61b7010b
Ali Nti <Ali3nti@gmail.com>
2024-11-19 20:10:31 +0330

    update

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends Controller
     
     public function product(Request $request)
     {
-        $product = DB::table('product')
+        $product = DB::table('products')
             ->where("id",$request->id)
             ->first();
 
